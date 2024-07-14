d8e8e2741f1af8cdb34c48340707afac7c708046
Chiter22888 <nureltop60@gmail.com>
2024-07-14 11:10:35 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,69 +1,56 @@
-if move == -1:
+import requests
+import time
+import json
 
-# If player can win, block him.
+TOKEN = '7315434185:AAGU2uA-m4qZOQQ16u6ug-IJByPsoN_sVXc'
+URL = 'https://api.telegram.org/bot'
 
-for i in range(1,10):
+def get_updates(offset=0):
+    result = requests.get(f'{URL}{TOKEN}/getUpdates?offset={offset}').json()
+    return result['result']
 
-if make_move(board, player, i, True)[1]:
+def send_message(chat_id, text):
+    requests.get(f'{URL}{TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
 
-move=i
+def reply_keyboard(chat_id, text):
+    reply_markup ={ "keyboard": [["Привет", "Hello"]], "resize_keyboard": True, "one_time_keyboard": True}
+    data = {'chat_id': chat_id, 'text': text, 'reply_markup': json.dumps(reply_markup)}
+    requests.post(f'{URL}{TOKEN}/sendMessage', data=data)
 
-break
+def check_message(chat_id, message):
+    if message.lower() in ['привет', 'hello']:
+        send_message(chat_id, 'Привет :)')
+    else:
+        reply_keyboard(chat_id, 'Я не понимаю тебя :(')
 
-if move == -1:
+def run():
+    update_id = get_updates()[-1]['update_id'] # Сохраняем ID последнего отправленного сообщения боту
+    while True:
+        time.sleep(2)
+        messages = get_updates(update_id) # Получаем обновления
+        for message in messages:
+            # Если в обновлении есть ID больше чем ID последнего сообщения, значит пришло новое сообщение
+            if update_id < message['update_id']:
+                update_id = message['update_id']# Сохраняем ID последнего отправленного сообщения боту
+                if (user_message := message['message'].get('text')): # Проверим, есть ли текст в сообщении
+                    check_message(message['message']['chat']['id'], user_message) # Отвечаем
 
-# Otherwise, try to take one of desired places.
-
-for tup in moves:
-
-for mv in tup:
-
-if move == -1 and can_move(board, computer, mv):
-
-move=mv
-
-break
-
-return make_move(board, computer, move)
-
-def space_exist():
-
-return board.count('X') + board.count('O') != 9
-
-player, computer = select_char()
-
-print('Player is [%s] and computer is [%s]' % (player, computer))
-
-result='%%% Deuce ! %%%'
-
-while space_exist():
-
-print_board()
-
-print('#Make your move ! [1-9] : ', end='')
-
-move = int(input())
-
-moved, won = make_move(board, player, move)
-
-if not moved:
-
-print(' >> Invalid number ! Try again !')
-
-continue
-
-if won:
-
-result='*** Congratulations ! You won ! ***'
-
-break
-
-elif computer_move()[1]:
-
-result='=== You lose ! =='
-
-break;
-
-print_board()
-
-print(result)
+if __name__ == '__main__':
+    run()
+def reply_keyboard(chat_id, text):
+    reply_markup ={ "keyboard": [["Привет", "Hello"], [{"request_location":True, "text":"Где я нахожусь"}]], "resize_keyboard": True, "one_time_keyboard": True}
+    data = {'chat_id': chat_id, 'text': text, 'reply_markup': json.dumps(reply_markup)}
+    requests.post(f'{URL}{TOKEN}/sendMessage', data=data)
+  def run():
+    update_id = get_updates()[-1]['update_id'] # Сохраняем ID последнего отправленного сообщения боту
+    while True:
+        time.sleep(2)
+        messages = get_updates(update_id) # Получаем обновления
+        for message in messages:
+            # Если в обновлении есть ID больше чем ID последнего сообщения, значит пришло новое сообщение
+            if update_id < message['update_id']:
+                update_id = message['update_id']# Сохраняем ID последнего отправленного сообщения боту
+                if (user_message := message['message'].get('text')): # Проверим, есть ли текст в сообщении
+                    check_message(message['message']['chat']['id'], user_message) # Отвечаем
+                if (user_location := message['message'].get('location')): # Проверим, если ли location в сообщении 
+                    print(user_location)
